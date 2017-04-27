f85c2e0eeff2c2df94c03b41db9f01498af58052
Megan Coyle <mcoyle@usgbc.org>
2017-04-27 13:52:12 -0400

    Additional webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-
 module.exports = {
-    entry: "./public/app.js",
+    entry: "./public/js/app.js",
     output: {
         path: "assets",
         filename: "bundle.min.js",
@@ -9,35 +7,31 @@ module.exports = {
     },
     module: {
         loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: ['babel'],
-                query: {
-                    presets: ['es2015', 'react', 'stage-0']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader!autoprefixer-loader'
-
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-       new OptimizeCssAssetsPlugin({
-           assetNameRegExp: /\.optimize\.css$/g,
-           cssProcessor: require('cssnano'),
-           cssProcessorOptions: {discardComments: {removeAll: true}},
-           canPrint: true
-       })
-   ]
+			       {
+               test: /\.jsx?$/,
+               exclude: /(node_modules|bower_components)/,
+               loader: 'babel'
+             },
+             {
+               test: /\.css$/,
+               loader: 'style-loader!css-loader'
+             },
+             {
+               test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+               loader: "file"
+             },
+             {
+               test: /\.(woff|woff2)$/,
+               loader:"url?prefix=font/&limit=5000"
+             },
+             {
+               test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+               loader: "url?limit=10000&mimetype=application/octet-stream"
+             },
+             {
+               test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+               loader: "url?limit=10000&mimetype=image/svg+xml"
+             }
+	       ]
+    }
 }
