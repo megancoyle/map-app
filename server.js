66aae4c9dad5e77bddfa929e0650b01b582fa1c2
Megan Coyle <mcoyle@usgbc.org>
2017-05-01 16:07:58 -0400

    Set up routes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse applica
 app.use(methodOverride());
 
 // Routes
-// require('./app/routes.js')(app);
+require('./app/routes.js')(app);
 
 // Listen
 app.listen(port);
